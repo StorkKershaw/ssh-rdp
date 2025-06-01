d8059e6c4e1c97ac6adb2026f5630d1600bfcab8
StorkKershaw <stork_kershaw@outlook.com>
2025-06-02 00:38:56 +0900

    build installer via zig build

--- build.zig
+++ build.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
+const config = .{
+    .app_name = "ssh-rdp",
+    .app_version = "0.0.3",
+    .app_publisher = "StorkKershaw",
+    .app_url = "https://github.com/StorkKershaw/ssh-rdp",
+};
+
 pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -14,7 +21,7 @@ pub fn build(b: *std.Build) void {
 
     const win32 = b.dependency("win32", .{});
     const server_exe = b.addExecutable(.{
-        .name = "ssh-rdpd",
+        .name = config.app_name ++ "d",
         .root_source_file = b.path("src/server.zig"),
         .target = target,
         .optimize = optimize,
@@ -23,15 +30,15 @@ pub fn build(b: *std.Build) void {
 
     const clap = b.dependency("clap", .{});
     const client_exe = b.addExecutable(.{
-        .name = "ssh-rdp",
+        .name = config.app_name,
         .root_source_file = b.path("src/client.zig"),
         .target = target,
         .optimize = optimize,
     });
     client_exe.root_module.addImport("clap", clap.module("clap"));
 
-    b.installArtifact(server_exe);
-    b.installArtifact(client_exe);
+    const server_install = b.addInstallArtifact(server_exe, .{});
+    const client_install = b.addInstallArtifact(client_exe, .{});
 
     const server_run = b.addRunArtifact(server_exe);
     const client_run = b.addRunArtifact(client_exe);
@@ -44,8 +51,24 @@ pub fn build(b: *std.Build) void {
         client_run.addArgs(args);
     }
 
-    const server_step = b.step("server", "Build and run `ssh-rdpd`.");
+    const server_step = b.step("server", "Build and run `" ++ config.app_name ++ "d`.");
     server_step.dependOn(&server_run.step);
-    const client_step = b.step("client", "Build and run `ssh-rdp`.");
+    const client_step = b.step("client", "Build and run `" ++ config.app_name ++ "`.");
     client_step.dependOn(&client_run.step);
+
+    const arguments: []const []const u8 = &.{
+        "ISCC.exe",
+        "/Qp",
+        b.fmt("/DAppName={s}", .{config.app_name}),
+        b.fmt("/DAppVersion={s}", .{config.app_version}),
+        b.fmt("/DAppPublisher={s}", .{config.app_publisher}),
+        b.fmt("/DAppURL={s}", .{config.app_url}),
+        b.fmt("/DOutputDir={s}\\bin", .{b.install_prefix}),
+        "installer.iss",
+    };
+    const installer = b.addSystemCommand(arguments);
+    installer.step.dependOn(&server_install.step);
+    installer.step.dependOn(&client_install.step);
+
+    b.getInstallStep().dependOn(&installer.step);
 }
---
+++ installer.iss
@@ -0,0 +1,44 @@
+;#define AppName ...
+;#define AppVersion ...
+;#define AppPublisher ...
+;#define AppURL ...
+;#define OutputDir ...
+
+[Setup]
+AppId={{4FE582AE-7EE2-49EF-B41D-1459C14A9DF2}}
+AppName={#AppName}
+AppVersion={#AppVersion}
+;AppVerName={#AppName} {#AppVersion}
+AppPublisher={#AppPublisher}
+AppPublisherURL={#AppURL}
+AppSupportURL={#AppURL}
+AppUpdatesURL={#AppURL}
+DefaultDirName={autopf}\{#AppName}
+DisableDirPage=yes
+UninstallDisplayIcon={app}\{#AppName}.exe
+; "ArchitecturesAllowed=x64compatible" specifies that Setup cannot run
+; on anything but x64 and Windows 11 on Arm.
+ArchitecturesAllowed=x64compatible
+; "ArchitecturesInstallIn64BitMode=x64compatible" requests that the
+; install be done in "64-bit mode" on x64 or Windows 11 on Arm,
+; meaning it should use the native 64-bit Program Files directory and
+; the 64-bit view of the registry.
+ArchitecturesInstallIn64BitMode=x64compatible
+DefaultGroupName={#AppName}
+DisableProgramGroupPage=yes
+PrivilegesRequired=lowest
+OutputDir={#OutputDir}
+OutputBaseFilename="{#AppName} v{#AppVersion}"
+SolidCompression=yes
+WizardStyle=modern
+
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+
+[Files]
+Source: "{#OutputDir}\{#AppName}.exe"; DestDir: "{app}"; Flags: ignoreversion
+Source: "{#OutputDir}\{#AppName}d.exe"; DestDir: "{app}"; Flags: ignoreversion
+; NOTE: Don't use "Flags: ignoreversion" on any shared system files
+
+[Icons]
+Name: "{group}\{#AppName}"; Filename: "{app}\{#AppName}.exe"
